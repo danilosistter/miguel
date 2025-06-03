1bef03be3cbda1a375a183336f8ce84d7fe95d76
danilosistter <130368347+danilosistter@users.noreply.github.com>
2025-06-02 21:10:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.innerText = "Obrigado por visitar nosso site! Vamos cuidar do planeta juntos! 🌍🌱";
+  mensagem.style.fontWeight = "bold";
+  mensagem.style.marginTop = "10px";
+}
